26c6fc939b4f004d7a31c598fd85e412bfd1ebb3
Derek <ushakov_gd@mail.ru>
2026-02-10 22:26:22 +0300

    Добавил ф-цию произведения 2-х чисел
    Добавил ф-ию вычисления факториала числа

--- GIT.py
+++ GIT.py
@@ -6,3 +6,16 @@ print(sum_numbers(5, 7))
 
 def multi_number (a: float, b: float) ->float:
     return a * b
+
+def factorial(n: int) ->int:
+    """Функция вычисления факториала числа"""
+
+    if n == 0:
+        return "Ошибка"
+    mult = 1
+    for i in range(1, n + 1):
+        mult *= i
+
+    return mult
+
+print(factorial(0))
