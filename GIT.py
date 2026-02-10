079c0682996f500c923556bcfd59686ee18ca237
Derek <ushakov_gd@mail.ru>
2026-02-10 22:17:08 +0300

    Добавил ф-цию произведения 2-х чисел

--- GIT.py
+++ GIT.py
@@ -1,5 +1,8 @@
-def sum_numbers (a: float, b: float) ->float:
-
+def sum_numbers(a: float, b: float) -> float:
     return a + b
 
+
 print(sum_numbers(5, 7))
+
+def multi_number (a: float, b: float) ->float:
+    return a * b
