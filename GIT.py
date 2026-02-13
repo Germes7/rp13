1bf5e4cf623eb46d367b5ccceda3059ff2e755c7
Derek <ushakov_gd@mail.ru>
2026-02-13 22:59:33 +0300

    Добавил процедуру Привет

--- GIT.py
+++ GIT.py
@@ -2,8 +2,6 @@ def sum_numbers(a: float, b: float) -> float:
     return a + b
 
 
-print(sum_numbers(5, 7))
-
 def multi_number (a: float, b: float) ->float:
     return a * b
 
@@ -22,3 +20,7 @@ def average_3(a: float, b: float, c: float) -> float:
 
     return (a + b + c) / 3
 
+def Hello():
+    return "Hello"
+
+print(Hello())
