c5f4da46d2990c3ae0a360d8eb372c3f953d3dca
Derek <ushakov_gd@mail.ru>
2026-02-14 03:48:28 +0300

    Изменил ф-ию периметра прямоугольника

--- perimeter.py
+++ perimeter.py
@@ -5,3 +5,11 @@ def square_perimeter(side: float) -> float:
 
     return "Стороны должны быть больше нуля"
 
+def rectangle_perimeter(height: float, width: float):
+    """Периметр прямоугольника"""
+
+    if height > 0 and width > 0: return 2 * (height + width)
+
+    return "Стороны должны быть больше нуля"
+
+
