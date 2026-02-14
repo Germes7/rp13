cd6d28335a52ed78ffeb5e391c1760f07a843c60
Derek <ushakov_gd@mail.ru>
2026-02-14 03:42:19 +0300

    Изменил название ф-й вычисления всех периметров

--- perimeter.py
+++ perimeter.py
@@ -1,18 +1,18 @@
-def square_area(side: float) -> float:
+def square_perimeter(side: float) -> float:
     """Периметр квадрата"""
 
     if side > 0: return side * 4
 
     return "Стороны должны быть больше нуля"
 
-def rectangle_area(height: float, width: float):
+def rectangle_perimeter(height: float, width: float):
     """Периметр прямоугольника"""
 
     if height > 0 and width > 0: return 2 * (height + width)
 
     return "Стороны должны быть больше нуля"
 
-def cirkle_area(radius: float):
+def cirkle_perineter(radius: float):
     """Периметр круга"""
 
     import math as m
