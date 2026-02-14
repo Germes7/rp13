8d57e272e13758383f94fd57fc5567901c6d8711
Derek <ushakov_gd@mail.ru>
2026-02-14 03:46:53 +0300

    Изменил ф-ию периметра квадрата

--- perimeter.py
+++ perimeter.py
@@ -5,17 +5,3 @@ def square_perimeter(side: float) -> float:
 
     return "Стороны должны быть больше нуля"
 
-def rectangle_perimeter(height: float, width: float):
-    """Периметр прямоугольника"""
-
-    if height > 0 and width > 0: return 2 * (height + width)
-
-    return "Стороны должны быть больше нуля"
-
-def cirkle_perineter(radius: float):
-    """Периметр круга"""
-
-    import math as m
-    if radius > 0: return 2 * m.pi * radius
-
-    return "Радиус должен быть больше нуля"
